d5c27913d7760c452b00c6c54d97da6cbbcaa621
antik <antik211209@gmail.com>
2025-07-28 22:42:30 +0200

    Create MainClass.cs

---
+++ MainClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass
+{
+
+    
+    private static void Main(string[] args)
+    {
+        Program clientSession = new Program();
+
+        clientSession.Init();
+    }
+}
